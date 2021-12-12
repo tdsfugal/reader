28aaf2264e87c4cb604de683e0529ccb055d6982
TDSFugal <tim.smith@fugaltech.com>
2021-12-12 00:49:40 -0500

    queryless calls

--- packages/reader-gql/src/connectors/news-api.js
+++ packages/reader-gql/src/connectors/news-api.js
@@ -1,21 +1,29 @@
-const content = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-];
+const axios = require("axios");
 
 class NewsApi {
   constructor() {
-    const apikey = process.env.API_KEY;
-  }
+    // The connector is constructed this way to hide the API_KEY. The "caller"
+    // instance of axios exists only within the closure of this constructor.
+    // This is not a perfect solution for making the key inaccesabile, but it
+    // does make it difficult enough that it will not be inadvertently exposed.
+    // The downside of this pattern is that all of the methods that use the
+    // caller instance must be defined inside the closure too.
+    const baseOptions = {
+      baseUrl: "https://newsapi.org",
+      headers: {
+        Accepts: "applications/json",
+        Authorization: process.env.API_KEY,
+      },
+    };
 
-  getContent() {
-    return content;
+    this.getContent = async () => {
+      try {
+        const options = { ...baseOptions };
+        const resp = await axios.get("/v2/everything", options);
+      } catch (e) {
+        console.log(e);
+      }
+    };
   }
 }
 
--- packages/reader-gql/src/connectors/news-api.test.js
+++ packages/reader-gql/src/connectors/news-api.test.js
@@ -1,5 +1,25 @@
+const axios = require("axios");
+
 const { NewsApi } = require("./news-api");
 
-test("compiles", () => {
-  expect(new NewsApi()).toBeInstanceOf(NewsApi);
+describe("connectors/news-api", () => {
+  test("compiles", () => {
+    expect(new NewsApi()).toBeInstanceOf(NewsApi);
+  });
+
+  test("makes get call with API_KEY as auth", async () => {
+    process.env.API_KEY = "Super Secret String";
+    const path = "/v2/everything";
+    const options = {
+      baseUrl: "https://newsapi.org",
+      headers: {
+        Accepts: "applications/json",
+        Authorization: "Super Secret String",
+      },
+    };
+    axios.get = jest.fn();
+    const nApi = new NewsApi();
+    const resp = await nApi.getContent();
+    expect(axios.get).toHaveBeenCalledWith(path, options);
+  });
 });
